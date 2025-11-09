07a3cd18fc0118e40de3350744b8565b0b8d133a
samalani77 <samgst17@gmail.com>
2025-11-09 11:03:57 +0530

    Implement basic arithmetic operations in script
    Replaced name input and greeting with arithmetic operations.

--- Assignment1_Task1.py
+++ Assignment1_Task1.py
@@ -1,5 +1,10 @@
-a = str(input("Enter your first name: "))
-b = str(input("Enter your second name: "))
-c= a + " " + b + "!"
-
-print("Hello,", c, "Welcome to the Python program.")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+Addition = a+b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+print("The Addition of two numbers is ", Addition)
+print("The Subtraction of two numbers is ", Subtraction)
+print("The product of two numbers is ", Multiplication)
+print("The Division of two numbers is ", Division)
